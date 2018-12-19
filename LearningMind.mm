6f5182f14522036c6a29a9d73af4fd54c338a3e5
Ju He <hejunion@gmail.com>
2018-12-18 21:07:13 -0500

    Create MindMap File

---
+++ LearningMind.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1545185165952" ID="ID_689157906" MODIFIED="1545185165952" TEXT="New Mindmap"/>
+</map>
