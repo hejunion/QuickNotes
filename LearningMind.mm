d7ba8654a5b246b36c9b1ff172933d5943a810c9
Ju He <hejunion@gmail.com>
2019-06-14 18:41:08 -0400

    AWS ...

--- LearningMind.mm
+++ LearningMind.mm
@@ -1,4 +1,12 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1545185165952" ID="ID_689157906" MODIFIED="1545185165952" TEXT="New Mindmap"/>
+<node CREATED="1545185165952" ID="ID_689157906" MODIFIED="1545185165952" TEXT="New Mindmap">
+<node CREATED="1553309713850" ID="ID_1263229802" MODIFIED="1553309720551" POSITION="right" TEXT="Course by UBC">
+<node CREATED="1553309725578" ID="ID_609428133" MODIFIED="1553309733570" TEXT="https://extendedlearning.ubc.ca/programs/immigration-laws-policies-procedures-certificate"/>
+</node>
+<node CREATED="1553309720801" ID="ID_259270578" MODIFIED="1553309875650" POSITION="right" TEXT="Reading Act">
+<node CREATED="1553309879285" ID="ID_1182413857" MODIFIED="1553309881944" TEXT="https://laws.justice.gc.ca/eng/acts/i-2.5/"/>
+</node>
+<node CREATED="1553309862004" ID="ID_108234613" MODIFIED="1553309862004" POSITION="left" TEXT=""/>
+</node>
 </map>
